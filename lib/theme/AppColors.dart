f3128d2393c49f6f80f3eb493365482f07ff2840
Oussama Boufari <oussamaboufari@gmail.com>
2024-10-14 23:15:53 +0100

    adding app colors and typoghraphy

---
+++ lib/theme/AppColors.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primary500 = Color(0xff06c149);
+
+  static const Color primary400 = Color(0xff38cd6d);
+
+  static const Color primary300 = Color(0xff6ada92);
+
+  static const Color primary200 = Color(0xff9be6b6);
+
+  static const Color primary100 = Color(0xffe6f9ed);
+
+  static const Color secondary500 = Color(0xffffd300);
+
+  static const Color secondary400 = Color(0xffffdc33);
+
+  static const Color secondary300 = Color(0xffffe566);
+
+  static const Color secondary200 = Color(0xffffed99);
+
+  static const Color secondary100 = Color(0xfffffbe6);
+
+  static const Color alertsStatusSuccess = Color(0xff06c149);
+
+  static const Color alertsStatusInfo = Color(0xff246bfd);
+
+  static const Color alertsStatusWarning = Color(0xfffacc15);
+
+  static const Color alertsStatusError = Color(0xfff75555);
+
+  static const Color alertsStatusDisabled = Color(0xffd8d8d8);
+
+  static const Color alertsStatusButtonDisabled = Color(0xff0d9e41);
+
+  static const Color greyscale900 = Color(0xff212121);
+
+  static const Color greyscale800 = Color(0xff424242);
+
+  static const Color greyscale700 = Color(0xff616161);
+
+  static const Color greyscale600 = Color(0xff757575);
+
+  static const Color greyscale500 = Color(0xff9e9e9e);
+
+  static const Color greyscale400 = Color(0xffbdbdbd);
+
+  static const Color greyscale300 = Color(0xffe0e0e0);
+
+  static const Color greyscale200 = Color(0xffeeeeee);
+
+  static const Color greyscale100 = Color(0xfff5f5f5);
+
+  static const Color greyscale50 = Color(0xfff9f9f9);
+
+  static const gradientsGradientGreen =
+      LinearGradient(colors: [Color(0xff06c149), Color(0xff1adf60)]);
+
+  static const gradientsGradientRed =
+      LinearGradient(colors: [Color(0xffe21221), Color(0xffff4351)]);
+
+  static const gradientsGradientPurple =
+      LinearGradient(colors: [Color(0xff7210ff), Color(0xff9d59ff)]);
+
+  static const gradientsGradientYellow =
+      LinearGradient(colors: [Color(0xfffacc15), Color(0xffffe57f)]);
+
+  static const gradientsGradientBlue =
+      LinearGradient(colors: [Color(0xff335ef7), Color(0xff5e81ff)]);
+
+  static const gradientsGradidentOrange =
+      LinearGradient(colors: [Color(0xfffb9400), Color(0xffffab38)]);
+
+  static const Color othersWhite = Color(0xffffffff);
+
+  static const Color othersBlack = Color(0xff000000);
+
+  static const Color othersRed = Color(0xfff54336);
+
+  static const Color othersPink = Color(0xffea1e61);
+
+  static const Color othersPurple = Color(0xff9d28ac);
+
+  static const Color othersDeepPurple = Color(0xff673ab3);
+
+  static const Color othersIndigo = Color(0xff3f51b2);
+
+  static const Color othersBlue = Color(0xff1a96f0);
+
+  static const Color othersLightBlue = Color(0xff00a9f1);
+
+  static const Color othersCyan = Color(0xff00bcd3);
+
+  static const Color othersTeal = Color(0xff009689);
+
+  static const Color othersGreen = Color(0xff4aaf57);
+
+  static const Color othersLightGreen = Color(0xff8bc255);
+
+  static const Color othersLime = Color(0xffcddc4c);
+
+  static const Color othersYellow = Color(0xffffeb4f);
+
+  static const Color othersAmber = Color(0xffffc02d);
+
+  static const Color othersOrange = Color(0xffff981f);
+
+  static const Color othersDeepOrange = Color(0xffff5726);
+
+  static const Color othersBrown = Color(0xff7a5548);
+
+  static const Color othersBlueGrey = Color(0xff607d8a);
+
+  static const Color darkDark1 = Color(0xff181a20);
+
+  static const Color darkDark2 = Color(0xff1f222a);
+
+  static const Color darkDark3 = Color(0xff35383f);
+
+  static const Color backgroundGreen = Color(0xffebfaf1);
+
+  static const Color backgroundRed = Color(0xfffff5f6);
+
+  static const Color backgroundPurple = Color(0xfff4ecff);
+
+  static const Color backgroundBlue = Color(0xfff6fafd);
+
+  static const Color backgroundOrange = Color(0xfffff7ed);
+
+  static const Color backgroundPink = Color(0xfffff5f5);
+
+  static const Color backgroundYellow = Color(0xfffffee0);
+
+  static const Color transparentGreen = Color(0x1406c149);
+
+  static const Color transparentRed = Color(0x14e21221);
+
+  static const Color transparentPurple = Color(0x147210ff);
+
+  static const Color transparentBlue = Color(0x14335ef7);
+
+  static const Color transparentOrange = Color(0x14ff9800);
+
+  static const Color transparentYellow = Color(0x14facc15);
+
+  static const Color transparentCyan = Color(0x1400bcd4);
+
+  static const Color materialThemeWhite = Color(0xffffffff);
+
+  static const Color materialThemeBlack = Color(0xff000000);
+}
